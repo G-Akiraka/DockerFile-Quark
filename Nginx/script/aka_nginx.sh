a93fee54b372fe4619a64d794c756763d5226cfc
akiraka <akiraka@outlook.com>
2020-12-11 12:19:12 +0800

    2

--- Nginx/script/aka_nginx.sh
+++ Nginx/script/aka_nginx.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-#   启动 crond 与 rsyslog 服务
-crond && rsyslogd
+# 启动 crond 与 rsyslog 服务
+if [ `cat /etc/issue | grep -i Alpine | wc -l` -eq 1 ];then
+  echo "是 Alpine 系统"
+  crond && rsyslogd
+else
+  echo " 非 Alpine 系统"
+  service cron start && service rsyslog start
+fi
 
 #   启动 nginx
 nginx -g "daemon off;"
